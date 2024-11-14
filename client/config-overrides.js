91f0facc5b5d14669f3d0841ab575be3f9a012e0
Josh-M2 <joshuamagdada@gmail.com>
2024-11-15 07:57:44 +0800

    update

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -2,7 +2,7 @@ const { override } = require("customize-cra");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = override((config) => {
-  if (process.env.REACT_APP_NODE_ENV === "production") {
+  if (config.mode === "production") {
     console.log("Webpack config in production mode:", config);
     config.optimization.minimizer = [
       new TerserPlugin({
